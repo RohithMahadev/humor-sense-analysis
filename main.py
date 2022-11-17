01084e29c3549244df37233f97dab03d3bd4e5e0
unknown <rohithmahadev30@gmail.com>
2022-11-17 19:26:46 +0530

    source code updated

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ hide_menu_style = """
         #MainMenu {visibility: hidden;}
         </style>
         """
-#st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
 image = Image.open('pic_1.png')
